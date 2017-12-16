9261d11c3ddbf846172b97a492dab9fd2f3ac017
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-16 11:33:58 +0100

    Day 10 - fix part 2
    Turns out I had misread the spec

--- 2017/10.2.py
+++ 2017/10.2.py
@@ -1,18 +1,17 @@
 input = '63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24'
-input = ''
 lengths = [ord(n) for n in input] + [17, 31, 73, 47, 23]
 pos, skip_size = 0, 0
 listylist = list(range(256))
-for length in lengths:
-    for x in range(64):
-        end = min(pos + length, len(listylist))
-        wrap_end  = max(pos + length - len(listylist), 0)
-        selection = listylist[pos:end] + listylist[0:wrap_end]
-        rev = selection[::-1]
-        listylist[pos:end] = rev[:end - pos]
-        listylist[0:wrap_end] = rev[end -pos:]
-        pos = (pos + length + skip_size) % len(listylist)
-        skip_size = skip_size + 1
+for x in range(64):
+    for length in lengths:
+            end = min(pos + length, len(listylist))
+            wrap_end  = max(pos + length - len(listylist), 0)
+            selection = listylist[pos:end] + listylist[0:wrap_end]
+            rev = selection[::-1]
+            listylist[pos:end] = rev[:end - pos]
+            listylist[0:wrap_end] = rev[end -pos:]
+            pos = (pos + length + skip_size) % len(listylist)
+            skip_size = skip_size + 1
 dense = [0 for i in range(16)]
 for dense_idx in range(16):
     for listy_idx in range(dense_idx * 16, dense_idx * 16 + 16):
