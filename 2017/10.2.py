a0efc761a29bd73742412e092ebb4f41d0fb4851
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-16 11:46:21 +0100

    Cosmetic change to 10.2.py

--- 2017/10.2.py
+++ 2017/10.2.py
@@ -1,3 +1,5 @@
+from operator import xor
+from functools import reduce
 input = '63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24'
 lengths = [ord(n) for n in input] + [17, 31, 73, 47, 23]
 pos, skip_size = 0, 0
@@ -12,9 +14,6 @@ for x in range(64):
             listylist[0:wrap_end] = rev[end -pos:]
             pos = (pos + length + skip_size) % len(listylist)
             skip_size = skip_size + 1
-dense = [0 for i in range(16)]
-for dense_idx in range(16):
-    for listy_idx in range(dense_idx * 16, dense_idx * 16 + 16):
-        dense[dense_idx] ^= listylist[listy_idx]
+dense = [reduce(xor, listylist[i*16:i*16+16]) for i in range(16)]
 ans = ''.join(hex(num)[2:].zfill(2) for num in dense)
 print(ans)
