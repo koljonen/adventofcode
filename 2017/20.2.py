2a030e92652113680b1b33b15449d9e8c37e1582
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-22 11:38:09 +0400

    Working solution for day 20 part 2
    Turns out I had misread the spec again: vel updates before pos ...

--- 2017/20.2.py
+++ 2017/20.2.py
@@ -1,6 +1,6 @@
 from numpy import roots
 from math import sqrt
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
 with open('/Users/joakimkoljonen/src/adventofcode/20.input', 'r') as file:
     input = file.read()
@@ -15,15 +15,24 @@ p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
 p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
 p=<3,0,0>, v=<1,0,0>, a=<0,0,0>
 '''
-#input = test_input2
+test_input3 = '''p=<1,3,2>, v=< 2,0,0>, a=<3,0,2>
+p=<10,0,-1>, v=< 0,1,0>, a=<2,0,3>
+p=<1,0,-1>, v=< 2,1,0>, a=<3,0,3>
+'''
+test_input4 = '''p=< -5,0,0>, v=< 2,0,0>, a=<-1,0,0>
+p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>
+'''
+#input = test_input4
+
+Particle = namedtuple('Particle', ['pos', 'vel', 'acc'])
 
 def parseline(line):
-    return [parsestuff(thing) for thing in line.split(', ')]
+    return Particle(*[parsestuff(thing) for thing in line.split(', ')])
 
 def parsestuff(thing):
     return [int(x) for x in thing[3:-1].split(',')]
 
-points = [parseline(line) for line in input.split('\n') if line != '']
+particles = [parseline(line) for line in input.split('\n') if line != '']
 
 def solvequadratic(pos, vel, acc):
     if acc == 0 and vel == 0:
@@ -35,14 +44,14 @@ def solvequadratic(pos, vel, acc):
         if sol * vel + pos == 0 and sol >= 0:
             return [int(sol)]
         return []
-    vel = vel - acc / 2.0
+    vel = vel + acc / 2.0
     acc = acc / 2.0
-    discriminant = (vel ** 2) - (4* acc * pos)
+    discriminant = (vel ** 2) - (4 * acc * pos)
     if discriminant < 0:
         return []
-    sol1 = int((-vel - sqrt(discriminant)) / (2 * acc))
-    sol2 = int((-vel + sqrt(discriminant)) / (2 * acc))
-    return [s for s in (sol1, sol2) if pos + s * vel + acc * s * s == 0 and s >= 0]
+    sol1 = (-vel - sqrt(discriminant)) / (2.0 * acc)
+    sol2 = (-vel + sqrt(discriminant)) / (2.0 * acc)
+    return [s for s in (sol1, sol2) if s >= 0]
 
 def getcommon(solutions):
     cands = [s for ss in solutions for s in ss]
@@ -50,32 +59,29 @@ def getcommon(solutions):
         return sols == 'ALL' or cand in sols
     return [c for c in cands if all(check(c, sols) for sols in solutions)]
 
-collisions = defaultdict(set)
-for idx1, point1 in enumerate(points):
-    for idx2, point2 in enumerate(points):
-        if idx2 <= idx1:
-            continue
-        sols = []
-        for coordinate in range(3):
-            #print([point1[i][coordinate] for i in range(3)])
-            #print([point2[i][coordinate] for i in range(3)])
-            p = [point1[i][coordinate] - point2[i][coordinate] for i in range(3)]
-            csols = solvequadratic(*p)
-            sols.append(csols)
-        commonsols = getcommon(sols)
-        if sols[0] and sols[1] and sols[2]:
-            print(sols)
-            print(point1)
-            print(point2)
-            print [
-                [point1[typ][coord] - point2[typ][coord] for coord in range(3)]
-                for typ in range(3)
-            ]
-        for s in commonsols:
-            collisions[s].add(idx1)
-            collisions[s].add(idx2)
+def getcollisions():
+    collisions = defaultdict(set)
+    for idx1, p1 in enumerate(particles):
+        for idx2, p2 in enumerate(particles):
+            if idx2 <= idx1:
+                continue
+            sols = []
+            for coordinate in range(3):
+                p = [p1[i][coordinate] - p2[i][coordinate] for i in range(3)]
+                csols = solvequadratic(*p)
+                sols.append(csols)
+            commonsols = getcommon(sols)
+            for s in commonsols:
+                collisions[s].add(idx1)
+                collisions[s].add(idx2)
+    return collisions
 
 
 def getans():
-    return collisions
+    dead = set()
+    for time, colliders in sorted(getcollisions().items()):
+        colliders = [p for p in colliders if p not in dead]
+        if len(colliders) > 1:
+            dead.update(colliders)
+    return len(particles) - len(dead)
 print(getans())
