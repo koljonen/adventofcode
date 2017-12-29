eb97c48022089c778df90af2cdf5bc5814148ea6
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-29 11:20:54 +0400

    Fix and simplify 24.1
    Some code for 24.2 had gotten in there.

--- 2017/24.1.py
+++ 2017/24.1.py
@@ -1,42 +1,27 @@
-from collections import defaultdict, deque
-import re
+from collections import defaultdict
 
 with open('/Users/joakimkoljonen/src/adventofcode/2017/24.input', 'r') as file:
     input = file.read()
 
-test_input = '''0/2
-2/2
-2/3
-3/4
-3/5
-0/1
-10/1
-9/10
-'''
+with open('/Users/joakimkoljonen/src/adventofcode/2017/24.test_input', 'r') as file:
+    test_input = file.read()
 #input = test_input
 
 components = [tuple(int(x) for x in i.split('/')) for i in input.split('\n') if i != '']
 component_dict = defaultdict(list)
-for component in components:
+for component in (tuple(int(x) for x in line.split('/')) for line in input.split('\n') if line != ''):
     component_dict[component[0]].append(component[1])
-    component_dict[component[1]].append(component[0])
+    if component[0] != component[1]:
+        component_dict[component[1]].append(component[0])
 
 def bestbridge(current_end, used):
-    max_score = 0
-    max_len = 0
-    for new_end in component_dict[current_end]:
-        if (current_end, new_end) not in used:
-            new_used = used | set([(current_end, new_end), (new_end, current_end)])
-            score, length = bestbridge(new_end, new_used)
-            score += current_end + new_end
-            length += 1
-            if (length, score) > (max_len, max_score):
-                max_score = score
-                max_len = length
-    return max_score, max_len
+    return max(
+        0 if (min(current_end, new_end), max(current_end, new_end)) in used
+        else current_end + new_end + bestbridge(
+            new_end,
+            used | set(((min(current_end, new_end), max(current_end, new_end)),))
+        )
+        for new_end in component_dict[current_end]
+    )
 
-def getans():
-    return bestbridge(0, set())
-
-ans = getans()
-print(ans)
+print(bestbridge(0, set()))
