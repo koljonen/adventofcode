3e05ea93f8823debaf8049884ec398587e8a979b
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-29 10:48:57 +0400

    Simplify day 23 part 2

--- 2017/23.2.py
+++ 2017/23.2.py
@@ -1,24 +1,6 @@
-cnt = 0
-b, c, f, h = 0, 0, 0, 0
+from math import sqrt
 
-b = 8100 + 100000
-c = b
-c += 17000
-
-while True: ##jnz 1 -23
-    f = 1
-    for d in range(2, b // 2): #jnz g -13
-        e = b // d
-        if b == e * d:
-            f = 0
-            break
-    if not f: #jnz f 2
-        h += 1
-    if not b - c: #jnz g 2
-        break #jnz 1 3
-    b += 17
-    if not 1: #jnz 1 -23
-        break
-
-
-print h
+print sum(
+    any(b == b // d * d for d in range(2, int(sqrt(b))))
+    for b in range(8100 + 100000, 8100 + 100000 + 17000 + 1, 17)
+)
