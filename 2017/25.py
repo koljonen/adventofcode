9f6e85d072df0cc3397ce28eb2f46d5b673720ed
Joakim Koljonen <joakim.koljonen@trustly.com>
2017-12-29 12:35:14 +0400

    Fix + simplify day 25

--- 2017/25.py
+++ 2017/25.py
@@ -1,66 +1,54 @@
 pos = 0
-tape = defaultdict(lambda: 0)
+tape = set()
 state = 'A'
 for i in range(12794428):
     if state == 'A':
-        if tape[pos] == 0:
-            tape[pos] = 1
+        if pos not in tape:
+            tape.add(pos)
             pos += 1
             state = 'B'
-        elif tape[pos] == 1:
-            tape[pos] = 0
+        else:
+            tape.remove(pos)
             pos -= 1
             state = 'F'
-
     elif state == 'B':
-        if tape[pos] == 0:
-            tape[pos] = 0
+        if pos not in tape:
             pos += 1
             state = 'C'
-        elif tape[pos] == 1:
-            tape[pos] = 0
+        else:
+            tape.remove(pos)
             pos += 1
             state = 'D'
-
     elif state == 'C':
-        if tape[pos] == 0:
-            tape[pos] = 1
+        if pos not in tape:
+            tape.add(pos)
             pos -= 1
             state = 'D'
-        elif tape[pos] == 1:
-            tape[pos] = 1
+        else:
             pos += 1
             state = 'E'
-
     elif state == 'D':
-        if tape[pos] == 0:
-            tape[pos] = 0
+        if pos not in tape:
             pos -= 1
             state = 'E'
-        elif tape[pos] == 1:
-            tape[pos] = 0
+        else:
+            tape.remove(pos)
             pos -= 1
             state = 'D'
-
     elif state == 'E':
-        if tape[pos] == 0:
-            tape[pos] = 0
+        if pos not in tape:
             pos += 1
             state = 'A'
-        elif tape[pos] == 1:
-            tape[pos] = 1
+        else:
             pos += 1
             state = 'C'
-
     elif state == 'F':
-        if tape[pos] == 0:
-            tape[pos] = 1
+        if pos not in tape:
+            tape.add(pos)
             pos -= 1
             state = 'A'
-        elif tape[pos] == 1:
-            tape[pos] = 1
+        else:
             pos += 1
             state = 'A'
 
-ans = sum(tape.values())
-print(ans)
+print(len(tape))
